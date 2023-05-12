dcae416ba249e83eb77afeaf8b6489f8d4c43af1
Raphael Fromentin <rafbodaha@gmail.com>
2023-05-12 11:29:28 +0200

    feat: Add User factory

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class User extends Authenticatable {
+
+    use HasFactory;
 
     // Allow any field to be inserted
     protected $guarded = [];
