d39b41e83e1a65f7afac5ec3c9940659c415f4e1
tobiaszk02 <84630828+tobiaszk02@users.noreply.github.com>
2023-05-15 14:30:28 +0200

    feat: Add login/registration menu (#12)

---
+++ resources/views/login.blade.php
@@ -0,0 +1,33 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <link rel="icon" type="image/svg+xml" href="{{ asset('svg/LOGOS_JEUNES_ICON.svg') }}">
+    <title>JEUNES 6.4</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <style type="text/css">
+
+    </style>
+</head>
+<body>
+<div>
+    <label>Connexion</label><br>
+    <form action="/auth" method="get">
+        <label for="email">Entrer votre adresse mail:<input type="email" name="email" required></label><br>
+        <label for="password">Entrer votre mot de passe:<input type="password" name="password" required></label><br>
+        <input type="submit" value="Se connecter">
+    </form>
+</div>
+<div>
+        <label>Inscription</label><br>
+        <form action="/register" method="get">
+            <label for="email">Email:<input type="email" name="email" required></label><br>
+            <label for="password">Mot de passe:<input type="password" name="password" required></label><br>
+            <label for="confitm">Confirmer le mot de passe:<input type="password" name="confirm" required></label><br>
+            <label for="first_name">Prénom:<input type="text" name="first_name" required></label><br>
+            <label for="last_name">Nom:<input type="text" name="last_name" required></label><br>
+            <label for="birth_date">Date de naissance:<input type="date" name="birth_date" required></label><br>
+            <input type="submit" value="S'inscrire">
+        </form>
+</div>
+</body>
+</html>
