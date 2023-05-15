498e2421eb237897b3c8a8ee3aa159dcd8405ab3
JordanViknar <74505993+JordanViknar@users.noreply.github.com>
2023-05-15 20:35:27 +0200

    feat: Finish home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,7 @@
 		<title>JEUNES 6.4</title>
 		<meta charset="utf-8">
 		<link rel="stylesheet" href="{{ asset('css/app.css') }}">
-		<style type="text/css">
-			
-		</style>
+		<link rel="stylesheet" href="{{ asset('css/home.css') }}">
 	</head>
 	<body>
 		<div id="top_bars">
@@ -26,7 +24,7 @@
 		<div class="content_space">
 			<div id="home_grid">
 				<div>
-					<h2>De quoi s'agit-il ?</h2>
+					<h1>De quoi s'agit-il ?</h1>
 					<p>
 					D'une opportunité : celle qu'un engagement quel qu'il soit puissue être considéré à sa juste valeur.</br>
 					Toute expérience est source d'enrichissement et doit être reconnu largement.</br>
@@ -35,7 +33,7 @@
 				</div>
 
 				<div>
-					<h2>À qui s'adresse-t'il ?</h2>
+					<h1>À qui s'adresse-t'il ?</h1>
 					<p>
 					À vous, jeunes entre 16 et 30 ans, qui vous êtes investis spontanément dans une association ou dans tout type d'action formelle ou informelle, et qui avez partagé de votre temps, de votre énergie, pour apporter un soutien, une aide, une compétence.</br></br>
 					À vous, responsables de structures ou référents d'un jour, qui avez croisé la route de ces jeune et avez bénéficié même ponctuellement de cette implication citoyenne !</br>
@@ -45,35 +43,40 @@
 
 				<div>
 					<p>À vous, employeurs, recruteurs en ressources humaines, représentants d'organismes de formation, qui recevez ces jeunes, pour un emploi, un stage, un cursus de qualification, pour qui le savoir-être constitue le premier fondement de toute capacité humaine.</p>
-					<h2>Cet engagement est une ressource à valoriser au fil d'un parcours en 3 étapes :</h2>
+					<h3>Cet engagement est une ressource à valoriser au fil d'un parcours en 3 étapes :</h3>
 				</div>
 			</div>
 
-			<div>
-				<div>
-					<div>
-						<h3>1ère étape</h3>
-						<h2>la valorisation</h2>
+			<div id="home_links_grid">
+				<a id="home_link_valorisation" class="home_box" href="/jeune">
+					<div class="home_box_top_part">
+						<p>1ère étape</p>
+						<p>la valorisation</p>
 					</div>
-					<p>Décrivez votre expérience et mettez en avant ce que vous avez retiré.</p>
-				</div>
+					<div class="home_box_bottom_part">
+						<p>Décrivez votre expérience et mettez en avant ce que vous avez retiré.</p>
+					</div>
+				</a>
 
-				<div>
-					<div>
-						<h3>2ème étape</h3>
-						<h2>la confirmation</h2>
+				<a id="home_link_confirmation" class="home_box" href="/referent">
+					<div class="home_box_top_part">
+						<p>2ème étape</p>
+						<p>la confirmation</p>
 					</div>
-					<p>Confirmez cette expérience et ce que vous avez pu constater au contact de ce jeune.</p>
-				</div>
+					<div class="home_box_bottom_part">
+						<p>Confirmez cette expérience et ce que vous avez pu constater au contact de ce jeune.</p>
+					</div>
+				</a>
 
-				<div>
-				<div>
-					<div>
-						<h3>3ème étape</h3>
-						<h2>la consultation</h2>
+				<a id="home_link_consultation" class="home_box" href="/consultant">
+					<div class="home_box_top_part">
+						<p>3ème étape</p>
+						<p>la consultation</p>
 					</div>
-					<p>Valider cet engagement en prenant en compte sa valeur.</p>
-				</div>
+					<div class="home_box_bottom_part">
+						<p>Validez cet engagement en prenant en compte sa valeur.</p>
+					</div>
+				</a>
 			</div>
 		</div>
 	</body>
