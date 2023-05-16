d3e66639f32008ba5c2779e59bc46dabdf68819b
JordanViknar <74505993+JordanViknar@users.noreply.github.com>
2023-05-16 10:23:18 +0200

    refactor: Better page handling (#16)

---
+++ resources/views/app_common.blade.php
@@ -0,0 +1,14 @@
+<div id="top_bars">
+	<div id="motto_bar">
+		<img id="logo" src="{{ asset('svg/LOGOS_JEUNES.svg') }}" height="100%" xmlns="http://www.w3.org/2000/svg"/>
+		<div id="right_side">
+			<h1 id="motto_text">{{ $message ?? ""}}</h1>
+		</div>
+	</div>
+	<div id="tab_bar">
+		<a id="jeune_button" href="/jeune">JEUNE</a>
+		<a id="referent_button" href="/referent">RÉFÉRENT</a>
+		<a id="consultant_button" href="/consultant">CONSULTANT</a>
+		<a id="partners_button" href="/partners">PARTENAIRES</a>
+	</div>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,10 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 	<head>
-		<link rel="icon" type="image/svg+xml" href="{{ asset('svg/LOGOS_JEUNES_ICON.svg') }}">
-		<title>JEUNES 6.4</title>
-		<meta charset="utf-8">
-		<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+		@include('head_common')
 		<link rel="stylesheet" href="{{ asset('css/home.css') }}">
 	</head>
 	<body>
-		<div id="top_bars">
-			<div id="motto_bar">
-				<img id="logo" src="{{ asset('svg/LOGOS_JEUNES.svg') }}" height="100%" xmlns="http://www.w3.org/2000/svg"/>
-				<div id="right_side">
-					<h1 id="motto_text">Pour faire de l'engagement une valeur</h1>
-				</div>
-			</div>
-			<div id="tab_bar">
-				<a id="jeune_button" href="/jeune">JEUNE</a>
-				<a id="referent_button" href="/referent">RÉFÉRENT</a>
-				<a id="consultant_button" href="/consultant">CONSULTANT</a>
-				<a id="partners_button" href="/partners">PARTENAIRES</a>
-			</div>
-		</div>
+		@include('app_common', ['message' => "Pour faire de l'engagement une valeur"])
 		<div class="content_space">
 			<div id="home_grid">
 				<div>
