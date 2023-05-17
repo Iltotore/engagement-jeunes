fe0e96104d27d43510cf64ed4d1c472c65c33858
Raphael Fromentin <rafbodaha@gmail.com>
2023-05-17 15:51:58 +0200

    fix: Motto toolbar responsiveness

--- resources/views/head_common.blade.php
+++ resources/views/head_common.blade.php
@@ -23,29 +23,21 @@
 		font-family: 'Arial', sans-serif;
 		overflow: auto;
 		background-image: radial-gradient(circle at top, rgba(250,250,250,1) 90%, rgba(210,210,210,1) 99%);
-		min-height: 100vh;
 	}
 
 	div#motto_bar {
-		display:inline-block;
+		display: flex;
+		flex-direction: row;
 		vertical-align: bottom;
 		text-align: left;
-		height: 12em;
-		width: 100vw;
+		height: 20%;
+		max-width: 100vw;
 		background-image: radial-gradient(circle at bottom 0% left 21%, var(--color1) , var(--dark-color) 20%);
 	}
 
 	img#logo {
 		height: 100%;
 		margin-left: 3em;
-	}
-
-	div#right_side {
-		display:inline-block;
-		vertical-align: bottom;
-		width: calc(100vw - 14em);
-		font-size: 1.7em;
-		color: white;
 	}
 
 	h1#motto_text {
@@ -55,6 +47,11 @@
 		font-weight:1;
 		margin-bottom: 0.5em;
 		margin-top: 0em;
+		margin-right: 0.7em;
+		margin-left: auto;
+		font-size: 1.7em;
+		color: white;
+		align-self: flex-end;
 	}
 
 	div#tab_bar {
