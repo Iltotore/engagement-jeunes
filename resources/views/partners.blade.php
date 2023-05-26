b6dafde1b8d3d88d9e98aa168185748210c1919e
JordanViknar <74505993+JordanViknar@users.noreply.github.com>
2023-05-26 10:30:55 +0200

    feat: Add CSS for partners page (#35)
    * feat: Add partners CSS (unfinished)
    
    The icons don't scale properly with the screen size yet.
    
    * feat: Dynamic logo display

--- resources/views/partners.blade.php
+++ resources/views/partners.blade.php
@@ -1,23 +1,28 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     @include('head_common')
+	<link rel="stylesheet" href="{{ asset('css/partners.css') }}">
 </head>
 <body>
-    @include('app_common', ['message' => "Je m'engage"])
+    @include('app_common', ['message' => "Des partenaires tout aussi engagés"])
     <p>
-        JEUNES 6.4 est un dispositif issu de la charte de l’engagement pour la
+        JEUNES 6.4 est un dispositif issu de la <a href="#">charte de l’engagement</a> pour la
         jeunesse signée en 2013 par des partenaires institutionnels...
     </p>
     <br>
-    <img src="{{ asset('img/republique-francaise.png') }}" alt="République Française" style="width:300px;height:200px"/>
-    <img src="{{ asset('img/region-aquitaine.png') }}" alt="Région Aquitaine" style="width:300px;height:200px"/>
-    <img src="{{ asset('img/pyrenees-atlantiques2.png') }}" alt="Pyrénées Atlantiques conseil général" style="width:300px;height:200px"/>
-    <img src="{{ asset('img/assurance-maladie.png') }}" alt="l'assurance maladie" style="width:300px;height:200px"/>
-    <img src="{{ asset('img/assise-de-la-jeunesse.png') }}" alt="Assises de la jeunesse" style="width:300px;height:200px"/>
-    <img src="{{ asset('img/caf-bearn.et.soule.png') }}" alt="Caf Béarn et Soule" style="width:100px;height:200px"/>
-    <img src="{{ asset('img/caf-pays-basque.png') }}" alt="Caf du Pays Basque et du Seignanx" style="width:100px;height:200px"/>
-    <img src="{{ asset('img/msa2.png') }}" alt="MSA" style="width:300px;height:200px"/>
-    <img src="{{ asset('img/universite-pau.png') }}" alt="Université de Pau et des pays de l'adour" style="width:300px;height:200px"/>
+	<div id="icon_zone">
+		<img src="{{ asset('img/republique-francaise.png') }}" alt="République Française"/>
+		<img src="{{ asset('img/region-aquitaine.png') }}" alt="Région Aquitaine"/>
+		<img src="{{ asset('img/pyrenees-atlantiques2.png') }}" alt="Pyrénées Atlantiques conseil général"/>
+		<img src="{{ asset('img/assurance-maladie.png') }}" alt="l'assurance maladie"/>
+		<img src="{{ asset('img/assise-de-la-jeunesse.png') }}" alt="Assises de la jeunesse"/>
+		<div>
+			<img src="{{ asset('img/caf-bearn.et.soule.png') }}" alt="Caf Béarn et Soule"/>
+			<img src="{{ asset('img/caf-pays-basque.png') }}" alt="Caf du Pays Basque et du Seignanx"/>
+		</div>
+		<img src="{{ asset('img/msa2.png') }}" alt="MSA"/>
+		<img src="{{ asset('img/universite-pau.png') }}" alt="Université de Pau et des pays de l'adour"/>
+	</div>
     <br>
     <p>
         ...qui ont décidé de mettre en commun leurs actions pour les jeunes
