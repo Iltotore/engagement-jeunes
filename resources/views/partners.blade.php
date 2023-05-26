5ef15c77797124d2c62fe9c8c5ace603db988cf3
JordanViknar <74505993+JordanViknar@users.noreply.github.com>
2023-05-26 10:27:44 +0200

    feat: Dynamic logo display

--- resources/views/partners.blade.php
+++ resources/views/partners.blade.php
@@ -11,17 +11,17 @@
     </p>
     <br>
 	<div id="icon_zone">
-		<img src="{{ asset('img/republique-francaise.png') }}" alt="République Française" height="200px"/>
-		<img src="{{ asset('img/region-aquitaine.png') }}" alt="Région Aquitaine" height="200px"/>
-		<img src="{{ asset('img/pyrenees-atlantiques2.png') }}" alt="Pyrénées Atlantiques conseil général" height="200px"/>
-		<img src="{{ asset('img/assurance-maladie.png') }}" alt="l'assurance maladie" height="200px"/>
-		<img src="{{ asset('img/assise-de-la-jeunesse.png') }}" alt="Assises de la jeunesse" height="200px"/>
+		<img src="{{ asset('img/republique-francaise.png') }}" alt="République Française"/>
+		<img src="{{ asset('img/region-aquitaine.png') }}" alt="Région Aquitaine"/>
+		<img src="{{ asset('img/pyrenees-atlantiques2.png') }}" alt="Pyrénées Atlantiques conseil général"/>
+		<img src="{{ asset('img/assurance-maladie.png') }}" alt="l'assurance maladie"/>
+		<img src="{{ asset('img/assise-de-la-jeunesse.png') }}" alt="Assises de la jeunesse"/>
 		<div>
-			<img src="{{ asset('img/caf-bearn.et.soule.png') }}" alt="Caf Béarn et Soule" style="width:100px;height:200px"/>
-			<img src="{{ asset('img/caf-pays-basque.png') }}" alt="Caf du Pays Basque et du Seignanx" style="width:100px;height:200px"/>
+			<img src="{{ asset('img/caf-bearn.et.soule.png') }}" alt="Caf Béarn et Soule"/>
+			<img src="{{ asset('img/caf-pays-basque.png') }}" alt="Caf du Pays Basque et du Seignanx"/>
 		</div>
-		<img src="{{ asset('img/msa2.png') }}" alt="MSA" height="200px"/>
-		<img src="{{ asset('img/universite-pau.png') }}" alt="Université de Pau et des pays de l'adour" height="200px"/>
+		<img src="{{ asset('img/msa2.png') }}" alt="MSA"/>
+		<img src="{{ asset('img/universite-pau.png') }}" alt="Université de Pau et des pays de l'adour"/>
 	</div>
     <br>
     <p>
