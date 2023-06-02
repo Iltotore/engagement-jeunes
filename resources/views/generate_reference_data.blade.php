2795a7101d3d5208d07ee393093220aa18914d5c
JordanViknar <74505993+JordanViknar@users.noreply.github.com>
2023-06-02 11:47:10 +0200

    feat: Light CSS styling

--- resources/views/generate_reference_data.blade.php
+++ resources/views/generate_reference_data.blade.php
@@ -1,29 +1,59 @@
-<!-- {{ Auth::user()->references; }}
-</br> -->
-<h1>Liste des références pour {{ Auth::user()->first_name }} {{ Auth::user()->last_name }}:</h1>
-<div id="reference_area">
-	@foreach(Auth::user()->references as $reference)
-		<div class="reference_list_member">
-			<h2>{{ $reference->area }} pendant {{ $reference->timeDuration() / (24*3600) }} jours</h2>
-			<div class="reference_content">
-				<label class="summary">Référent : {{ $reference->ref_first_name }} {{ strtoupper($reference->ref_last_name) }}</label><br>
-				<label class="description_summary">Description :</label>
-				<p>{{ $reference->description }}</p>
-				<label>Lieu: {{ $reference->area }}</label><br>
-    			<label>Durée: {{ $reference->timeDuration() / (24*3600) }} jours.</label><br>
-				<label>Savoir-faire:</label>
-				<ul>
-					@foreach($reference->hardSkills() as $skill)
-						<li>{{ $skill }}</li>
-					@endforeach
-				</ul><br>
-				<label>Savoir-être:</label>
-				<ul>
-					@foreach($reference->softSkills() as $skill)
-						<li>{{ $skill }}</li>
-					@endforeach
-				</ul><br>
-			</div>
-		</div>
-	@endforeach
-</div>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+	<head>
+		<style>
+			* {
+				font-family: Arial, Helvetica, sans-serif;
+			}
+
+			h1 {
+				color: rgb(218, 8, 89);
+			}
+
+			label {
+				font-weight: bold
+			}
+
+			div.reference_content {
+				margin-left: 20px;
+			}
+
+			mark {
+				background-color: rgb(218, 8, 89);
+				color: white;
+				padding: 5px;
+			}
+		</style>
+	</head>
+	<body>
+		<!-- {{ Auth::user()->references; }}
+		</br> -->
+
+		<h1>Liste des références pour <mark>{{ Auth::user()->first_name }} {{ Auth::user()->last_name }}</mark>:</h1>
+		<ol id="reference_area">
+			@foreach(Auth::user()->references as $reference)
+				<li class="reference_list_member">
+					<h2>{{ $reference->area }} - {{ $reference->timeDuration() / (24*3600) }} jours</h2>
+					<div class="reference_content">
+						<label class="summary">Référent : </label>{{ $reference->ref_first_name }} {{ strtoupper($reference->ref_last_name) }}<br>
+						<label class="description_summary">Description :</label>
+						<p>{{ $reference->description }}</p>
+						<label>Lieu : </label>{{ $reference->area }}<br>
+						<label>Durée : </label>{{ $reference->timeDuration() / (24*3600) }} jours<br>
+						<label>Savoir-faire:</label>
+						<ul>
+							@foreach($reference->hardSkills() as $skill)
+								<li>{{ $skill }}</li>
+							@endforeach
+						</ul>
+						<label>Savoir-être:</label>
+						<ul>
+							@foreach($reference->softSkills() as $skill)
+								<li>{{ $skill }}</li>
+							@endforeach
+						</ul><br>
+					</div>
+				</li>
+			@endforeach
+		</ol>
+	</body>
+</html>
