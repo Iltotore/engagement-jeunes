4d192a6902dbb7fbdfac08ec2fd1445c6f2b2766
UnderSkyle <59476376+UnderSkyle@users.noreply.github.com>
2023-06-08 17:45:42 +0200

    feat: add scheduling to clear database (#71)

---
+++ app/Jobs/AutoDelete.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Consult;
+use App\Models\Reference;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class AutoDelete implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        foreach (User::all() as $user) {
+            if ($user->hasExpired()) {
+                $user->booted();
+                $user->delete();
+            }
+        }
+        foreach (Reference::all() as $ref){
+            if($ref->hasExpired()){
+                $ref->booted();
+                $ref->delete();
+            }
+        }
+        foreach (Consult::all() as $consult){
+            if($consult->hasEpired()){
+                $consult->booted();
+                $consult->delete();
+            }
+        }
+    }
+}
