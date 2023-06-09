1c3207d21fc94adbb03e5d96d8c2d1788d4dff4f
JordanViknar <74505993+JordanViknar@users.noreply.github.com>
2023-06-09 15:02:14 +0200

    feat: Add settings CSS (#75)

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -1,26 +1,27 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    @include('head_common')
-</head>
-<body>
-@include('app_common', ['message' => "Mon profil"])
-
-<div>
-    <form action="/api/settings" method="post">
-        @csrf
-        <fieldset>
-            <legend>Paramètres</legend>
-            <label for="email">Email:<input type="email" name="email" value="{{ Auth::user()->email }}" required></label><br>
-            <label for="password">Mot de passe :<input type="password" name="password" minlength="8" required></label><br>
-            <label for="new_password">Nouveau mot de passe :<input type="password" name="new_password" minlength="8" ></label><br>
-            <label for="confirm">Confirmer le nouveau mot de passe:<input type="password" name="confirm" ></label><br>
-            <label for="first_name">Prénom:<input type="text" name="first_name" value="{{ Auth::user()->first_name }}" required></label><br>
-            <label for="last_name">Nom:<input type="text" name="last_name" value="{{ Auth::user()->last_name }}" required></label><br>
-            <label for="birth_date">Date de naissance:<input type="date" name="birth_date" value="{{ Auth::user()->birth_date }}" required></label><br>
-            <input type="submit" value="Modifier">
-        </fieldset>
-    </form>
-</div>
-
-</body>
+	<head>
+		@include('head_common')
+		<link rel="stylesheet" href="{{ asset('css/settings.css') }}">
+	</head>
+	<body>
+		@include('app_common', ['message' => "Mon profil"])
+		<div id="settings_area">
+			<form id="settings_area" action="/api/settings" method="post">
+				@csrf
+				<fieldset>
+					<legend>Paramètres</legend>
+					<label for="email">Email : <input type="email" name="email" value="{{ Auth::user()->email }}" required></label><br>
+					</br>
+					<label for="password">Mot de passe : <input type="password" name="password" minlength="8" required></label><br>
+					<label for="new_password">Nouveau mot de passe : <input type="password" name="new_password" minlength="8" ></label><br>
+					<label for="confirm">Confirmer le nouveau mot de passe : <input type="password" name="confirm" ></label><br>
+					</br>
+					<label for="first_name">Prénom : <input type="text" name="first_name" value="{{ Auth::user()->first_name }}" required></label><br>
+					<label for="last_name">Nom : <input type="text" name="last_name" value="{{ Auth::user()->last_name }}" required></label><br>
+					<label for="birth_date">Date de naissance : <input type="date" name="birth_date" value="{{ Auth::user()->birth_date }}" required></label><br>
+					<input type="submit" value="Modifier">
+				</fieldset>
+			</form>
+		</div>
+	</body>
 </html>
