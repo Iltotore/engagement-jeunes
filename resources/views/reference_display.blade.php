168c4b3522cc36348627d34f41c6ebb6bae33fe0
UnderSkyle <59476376+UnderSkyle@users.noreply.github.com>
2023-06-09 18:06:09 +0200

    feat: added birthdate and name modification for referent (#76)
    Co-authored-by: Iltotore <rafbodaha@gmail.com>

--- resources/views/reference_display.blade.php
+++ resources/views/reference_display.blade.php
@@ -10,12 +10,18 @@
     <input type="hidden" name="token" value="{{ $reference->token }}">
     <input type="text" name="hard_skills">
     <input type="text" name="soft_skills">
+    <input type="text" name="ref_first_name">
+    <input type="text" name="ref_last_name">
+    <input type="date" name="ref_birth_date">
 </form>
 <label>Jeune: {{ $reference->user->first_name }} {{ $reference->user->last_name }}</label><br>
 <label>Description:</label><br>
 <p>{{ $reference->description }}</p>
 <label>Lieu: {{ $reference->area }}</label><br>
 <label>Durée: {{ $reference->timeDuration() / (24*3600) }} jours</label><br>
+<label for="ref_first_name">Prénom du référent :<input type="text" name="ref_first_name" required value="{{ $reference->ref_first_name }}"></label><br>
+<label for="ref_last_name">Nom du référent : <input type="text" name="ref_last_name" required value="{{ $reference->ref_last_name }}"></label><br>
+<label for="ref_birth_date">Date de naissance du référent : <input type="date" name="ref_birth_date" required value="{{ $reference->ref_birth_date }}"></label><br>
 <div>
     <label>Savoir-faire:</label>
     <input name="hard_skill" type="text">
@@ -45,6 +51,5 @@
     <input type="hidden" name="token" value="{{ $reference->token }}">
     <input type="submit" value="Confirmer la référence">
 </form>
-</div>
 </body>
 </html>
