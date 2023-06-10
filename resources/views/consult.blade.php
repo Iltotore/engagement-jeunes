4c278b032570c4caaf2b99b0f5b1ae4684dae2c4
JordanViknar <74505993+JordanViknar@users.noreply.github.com>
2023-06-10 15:50:58 +0200

    feat: Add CSS style for consult page

--- resources/views/consult.blade.php
+++ resources/views/consult.blade.php
@@ -1,37 +1,42 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    @include('head_common')
-</head>
-<body>
-@include('app_common', ['message' => "Références de ".$consult->user->first_name])
-<div>
-    <div class="user_info">
-        <label>Jeune: {{ $consult->user->first_name }} {{ $consult->user->last_name }}</label>
-        <label>Email: {{ $consult->user->email }}</label>
-        <label>Date de naissance: {{ $consult->user->birth_date }}</label>
-    </div>
-    <div class="reference_list">
-        @foreach($consult->references as $reference)
-            <div class="reference">
-                <label>Lieu: {{ $reference->area }}</label>
-                <label>Référent: {{ $reference->ref_first_name }} {{ $reference->ref_last_name }}</label>
-                <label>Durée: {{ $reference->timeDuration() / (24*3600) }} jours</label>
-                <label>Description:</label>
-                <p>{{ $reference->description }}</p>
-                <label>Savoir-faire:</label>
-                <ul>
-                    @foreach($reference->hardSkills() as $skill)
-                        <li>{{ $skill }}</li>
-                    @endforeach
-                </ul>
-                <label>Savoir-être:</label>
-                <ul>
-                    @foreach($reference->softSkills() as $skill)
-                        <li>{{ $skill }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endforeach
-    </div>
-</div>
-</body>
+
+	<head>
+		@include('head_common')
+		<link rel="stylesheet" href="{{ asset('css/consult.css') }}">
+	</head>
+
+	<body>
+		@include('app_common', ['message' => "Références de ".$consult->user->first_name])
+		<div id="consult_zone">
+			<fieldset class="user_info">
+				<legend>Informations sur le compte</legend>
+				<label class="static_text">Jeune : </label><label>{{ $consult->user->first_name }} {{ $consult->user->last_name }}</label></br>
+				<label class="static_text">Email : </label><label>{{ $consult->user->email }}</label></br>
+				<label class="static_text">Date de naissance : </label><label>{{ $consult->user->birth_date }}</label></br>
+			</fieldset>
+			<div class="reference_list">
+				@foreach($consult->references as $reference)
+					<fieldset class="reference">
+						<legend>{{ $reference->area }} - Référent : {{ $reference->ref_first_name }} {{ $reference->ref_last_name }}</legend>
+						<label class="static_text">Durée : </label><label>{{ $reference->timeDuration() / (24*3600) }} jours</label></br>
+						<label class="static_text">Description :</label>
+						<p>{{ $reference->description }}</p></br>
+						<label class="static_text">Savoir-faire :</label>
+						<ul>
+							@foreach($reference->hardSkills() as $skill)
+								<li>{{ $skill }}</li>
+							@endforeach
+						</ul>
+						<label class="static_text">Savoir-être :</label>
+						<ul>
+							@foreach($reference->softSkills() as $skill)
+								<li>{{ $skill }}</li>
+							@endforeach
+						</ul>
+					</fieldset>
+				@endforeach
+			</div>
+		</div>
+	</body>
+
+</html>
