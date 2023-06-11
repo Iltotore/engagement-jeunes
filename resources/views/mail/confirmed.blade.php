698e2900aff2c82f607d71050a864630bd526f63
Raphaël Fromentin <42907886+Iltotore@users.noreply.github.com>
2023-06-11 16:12:40 +0200

    feat: add confirm mail (#96)
    Co-authored-by: tobiasz <tobiaszek2002@gmail.com>

---
+++ resources/views/mail/confirmed.blade.php
@@ -0,0 +1,9 @@
+<body>
+    <p>
+        Bonjour {{ $reference->user->first_name }}.<br>
+
+        Votre demande de référence envoyée à {{ $reference->ref_first_name }} {{ $reference->ref_last_name }} a été confirmée.<br>
+
+        Consultez-la sur {{ \Illuminate\Support\Facades\URL::to("/account") }}
+    </p>
+</body>
